229916b0752dc41cc23e510bf38e39a4168f4c35
mansi06bhosale <mansi06bhosale@gmail.com>
2026-03-27 15:51:14 +0530

    Create task2.py

---
+++ Assignment 3 /task2.py
@@ -0,0 +1,11 @@
+#task2 math module for calculations
+
+import math
+num=int(input("Enter a number: "))
+res1=math.sqrt(num)
+res2=math.log(num)
+res3=math.sin(num)
+
+print(f"Squareroot: {res1}")
+print(f"Logarithm: {res2}")
+print(f"Sine: {res3}")
